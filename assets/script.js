e6e044c5e80e53ac5b6723e00ec6218067092c77
Jessica Dias <31523729+saidjess@users.noreply.github.com>
2022-11-22 16:48:50 -0300

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -1,61 +1,74 @@
-window.onload = function() {
-    cats();
-    
-    async function cats(){
-      await fetch("https://api.thecatapi.com/v1/images/search", {
+window.onload = function () {
+  let addToCartForm = document.querySelector('form[action$="/cart/add"]');
+  let formData = new FormData(addToCartForm);
+  fetch(window.Shopify.routes.root + 'cart/add.js', {
+    method: 'POST',
+    body: formData
+  })
+    .then(response => {
+      return response.json();
+    })
+    .catch((error) => {
+      console.error('Error:', error);
+    });
+
+  cats();
+
+  async function cats() {
+    await fetch("https://api.thecatapi.com/v1/images/search", {
+      method: "GET",
+      headers: { "Content-Type": "application/json" }
+    }).then(function (response) {
+      const data = response.json();
+
+      data.then(function (res) {
+        let html = "";
+        html += '<div class="title"><h2>Imagens aleatórias de gatinhos</h2></div>';
+        res.forEach(function (item, index) {
+          html += '<div class="imagem"><img src="' + item.url + '" alt=""/></div>';
+        });
+
+        document.getElementById("apiCats").innerHTML = html;
+      })
+    })
+      .catch(function (res) {
+        console.log(res);
+      });
+  }
+
+  posts();
+
+  async function posts() {
+    const token = "IGQVJVUHdEeEJySno0NjQ1TVBXYlZAjbjRKUkFucVVIcllJb2tjNVlSdWNvYzZAKajZAodFB1cHZAUZAjhZAZAHhkdFNCVXNBeXlPOHlYcVpJQkc5RWVKcHVxbDIwN1EwSVV5anhMWXpiVDh4ZAjNlelFvT1ZAiSwZDZD";
+    const url = "https://graph.instagram.com/me/media?access_token=" + token + "&fields=media_url,caption,permalink";
+
+    try {
+      await fetch(url, {
         method: "GET",
         headers: { "Content-Type": "application/json" }
-      }).then(function(response) {
-          const data = response.json();
-          
-          data.then(function(res) {            
-            let html = "";
-            html += '<div class="title"><h2>Imagens aleatórias de gatinhos</h2></div>';
-            res.forEach(function(item, index){
-                html +='<div class="imagem"><img src="'+item.url+'" alt=""/></div>';
-            });
-          
-            document.getElementById("apiCats").innerHTML = html;
-          })
+      }).then(function (response) {
+        const data = response.json();
+
+        data.then(function (res) {
+          let html = "";
+          html += '<div class="title"><h2>Siga nosso instagram</h2></div>';
+          res.data.forEach(function (item, index) {
+            if (index < 5) {
+              html += '<div class="item"><a href="' + item.permalink + '""><div class="imagem" style="background-image: url(' + item.media_url + ');"></div></a></div>';
+            }
+          });
+
+          document.getElementById("instagramPosts").innerHTML = html;
         })
-        .catch(function(res) {
-          console.log(res);
-        });
+      })
+    } catch (error) {
+      console.log(error)
     }
-  
-    posts();
-  
-    async function posts(){
-      const token = "IGQVJVUHdEeEJySno0NjQ1TVBXYlZAjbjRKUkFucVVIcllJb2tjNVlSdWNvYzZAKajZAodFB1cHZAUZAjhZAZAHhkdFNCVXNBeXlPOHlYcVpJQkc5RWVKcHVxbDIwN1EwSVV5anhMWXpiVDh4ZAjNlelFvT1ZAiSwZDZD";
-      const url = "https://graph.instagram.com/me/media?access_token="+token+"&fields=media_url,caption,permalink";
-      
-        try {
-          await fetch(url, {
-            method: "GET",
-            headers: { "Content-Type": "application/json" }
-          }).then(function(response) {
-            const data = response.json();
+  }
 
-            data.then(function(res) {
-                let html = "";
-                html += '<div class="title"><h2>Siga nosso instagram</h2></div>';
-                res.data.forEach(function(item, index){
-                  if(index < 5){
-                      html +='<div class="item"><a href="'+item.permalink+'""><div class="imagem" style="background-image: url('+item.media_url+');"></div></a></div>';
-                  }
-                });
-              
-                document.getElementById("instagramPosts").innerHTML = html;
-            })
-          })
-        } catch (error) {
-          console.log(error)
-        }
-    }
-
-    $('#itens__aboutUs').slick({
-      infinite: true,
-      slidesToShow: 1,
-      slidesToScroll: 1
-    });
+  // $('#itens__aboutUs').slick({
+  //   infinite: true,
+  //   slidesToShow: 1,
+  //   slidesToScroll: 1
+  // });
 };
